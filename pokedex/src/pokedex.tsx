10397826c7087fd4b43b943eda9bfdd8c356dfca
Oscar Gonzalez <gonzalez.gamboa1@gmail.com>
2024-04-29 02:06:55 -0600

    feat: Retrive Pokemons by Type seleted and set a limit of pokemons displayed to 12

--- pokedex/src/pokedex.tsx
+++ pokedex/src/pokedex.tsx
@@ -6,20 +6,85 @@ interface Type {
     url: string;
 }
 
+interface Pokemon {
+    name: string;
+    image: string;
+}
+
+interface PokemonProps {
+    types: Type[];
+    getPokemonsByType: (url: string) => void;   
+}
+
 export default function Pokedex(){
     const [types, setTypes] = useState<Type[]>([]);
+    const [typeSelected, setTypeSelected] = useState<string>('');
+    const [pokemons, setPokemons] = useState<Pokemon[]>([]);
 
-    //Tyoes Sekect Component
-    const TypesSelector = ({types}: {types: Type[]}) => {
+    //Types Select Inline Component
+    const TypesSelector = ({types, getPokemonsByType}: PokemonProps) => {
+
+        const setPokemonType = (e: React.ChangeEvent<HTMLSelectElement>) => {
+            let valueType = e.target.value;
+            setTypeSelected(valueType);
+
+            getPokemonsByType(valueType);
+            //console.log(valueType);
+        }
+
         return (
-            <select>
+            <select value={typeSelected} onChange={(e) => {setPokemonType(e)}}>
                 {
                     types.map((type, index) => {
-                        return <option key={index} value={type.name}>{type.name}</option>
+                        return <option key={index} value={type.url}>{type.name}</option>
                     })
                 }
             </select>
         )
+    }
+
+    const getPokemonsByType = async (url: string) => {
+        try {
+            const response = await fetch(url);
+            const pokemonsByType = await response.json();
+
+            const newPokemons: Pokemon[] = [];
+            
+            // for (const pokemon of pokemonsByType.pokemon) {
+            //     const response = await fetch(pokemon.pokemon.url);
+            //     const pokemonData = await response.json();
+
+            //     const newpokemon = {
+            //         name: pokemonData.name,
+            //         image: pokemonData.sprites.front_default
+            //     };
+
+            //     newPokemons.push(newpokemon);
+            // }
+
+            for (let i = 0; i < 12; i++) {
+                const response = await fetch(pokemonsByType.pokemon[i].pokemon.url);
+                const pokemonData = await response.json();
+
+                const newpokemon = {
+                    name: pokemonData.name,
+                    image: pokemonData.sprites.front_default
+                };
+
+                newPokemons.push(newpokemon);
+            }
+
+            //setPokemons(prevPokemons => [...prevPokemons, ...newPokemons]);
+            setPokemons(newPokemons);
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const checkCurrentPokemons = () => {
+        console.log(pokemons);
+        console.log(pokemons.length);
     }
 
     useEffect(() => {
@@ -36,7 +101,14 @@ export default function Pokedex(){
                 <div className='container'>
                     <h1>Pokedex</h1>
                     <label>Select the Pokemon Type</label>
-                    <TypesSelector types={types} />
+                    <TypesSelector types={types} getPokemonsByType={getPokemonsByType}/>
+                    <button onClick={() => {checkCurrentPokemons()}}>Console</button>
+                </div>
+            </div>
+            <div className='pokedex-grid'>
+                <div className='container'>
+                    <div>Prueba</div>
+                    <div>Prueba</div>
                 </div>
             </div>
         </>
